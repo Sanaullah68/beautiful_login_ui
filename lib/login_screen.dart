ebee70db8ff72b9e7a19335662554f5c69fa6647
Sanaullah <sanaullah5099@gmail.com>
2025-03-21 23:25:10 +0500

    adding images and set up of the project

---
+++ lib/login_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              Image.asset('assets/images/signin_balls.png'),
+              const Text(
+                "Sign In.",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 50,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
