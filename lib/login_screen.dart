19307eac90498b878aff59f707c6c54eff6e740d
Sanaullah <sanaullah5099@gmail.com>
2025-03-23 00:13:58 +0500

    added buttons and text field

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_login_ui/widgets/gradient_button.dart';
+import 'package:responsive_login_ui/widgets/login_field.dart';
+import 'package:responsive_login_ui/widgets/social_button.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -17,7 +20,49 @@ class LoginScreen extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                   fontSize: 50,
                 ),
-              )
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              const SocialButton(
+                  iconPath: 'assets/svgs/g_logo.svg',
+                  label: 'Continue with Google'),
+              const SizedBox(
+                height: 15,
+              ),
+              const SocialButton(
+                iconPath: 'assets/svgs/f_logo.svg',
+                label: 'Continue with Facebook',
+                horizontalPadding: 93,
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              const Text(
+                'or',
+                style: TextStyle(
+                  fontSize: 17,
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              const LoginField(
+                hintText: 'Email',
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              const LoginField(
+                hintText: 'Password',
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const GradientButton(),
+              const SizedBox(
+                height: 20,
+              ),
             ],
           ),
         ),
